63658c2901bd798ec98b4c9c26bf1840738e7092
Diyorbek <dsalimov816@gmail.com>
2026-04-15 12:25:33 +0500

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,53 @@
+#1-chisi
+# class car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+
+# car1 = car ("yasash", "BYD", "2026" )
+# print (car1.make)
+
+
+# 2-chisi
+# class telefon:
+#     def __init__(self, model, year ):
+#         self.model = model
+#         self.year = year
+
+
+# telefon1 = telefon("S22","2022")
+
+# print(telefon1.model)
+# print(telefon1.year)
+        
+
+# Qo'shimcha ish 
+
+# class dog:
+#     def __init__(self, name, year ):
+#         self.name = name 
+#         self.year = year
+
+
+# dog1 = dog ("laycha" , " 5 ")
+
+# print (dog1.name )
+# print (dog1.year)
+
+
+#qoshimcha
+# class dori:
+#     def __init__(self,name, turi):
+#         self.turi = turi
+#         self.name = name
+      
+
+
+
+# dor = dori ( "bosh og'rig'i","parasitamol")
+
+# print(dor.turi)
+# print(dor.name)
+
+
